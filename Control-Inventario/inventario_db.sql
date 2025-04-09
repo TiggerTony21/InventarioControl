a7a784b297a6aebfd2cdcb9f87b2be991287fa6b
TiggerTony21 <dickussobiggus@gmail.com>
2025-04-08 21:32:22 -0600

    Delete Control-Inventario directory

--- Control-Inventario/inventario_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE inventario_db;
-CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_producto VARCHAR(255) NOT NULL,
-    cantidad INT NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL
-);
